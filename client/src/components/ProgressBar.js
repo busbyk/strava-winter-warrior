fddf748eb8474813f855184161cc3325c86b7ff8
Kellen Busby <kellenbusby@gmail.com>
2022-01-01 21:03:56 -0500

    fixing the progress bar

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,12 +1,22 @@
+import isAfter from 'date-fns/isAfter'
+import isBefore from 'date-fns/isBefore'
+import formatDistance from 'date-fns/formatDistance'
+
+const CHALLENGE_START_DATE = process.env.REACT_APP_CHALLENGE_START_DATE
+const CHALLENGE_END_DATE = process.env.REACT_APP_CHALLENGE_END_DATE
+
 export default function ProgressBar(props) {
   const max = 31
   const today = new Date()
   const current = today.getDate()
+
+  const isWithinChallengeDates =
+    isAfter(today, new Date(CHALLENGE_START_DATE)) &&
+    isBefore(today, new Date(CHALLENGE_END_DATE))
+
   let displayVal
-  if (today.getMonth() === 0 && today.getFullYear() === 2021) {
-    displayVal = `${current} days down, ${
-      max - current
-    } days to go! Finish strong!`
+  if (isWithinChallengeDates) {
+    displayVal = `${formatDistance(today, new Date(CHALLENGE_END_DATE))} left!`
   } else {
     displayVal = 'Challenge has ended!'
   }
